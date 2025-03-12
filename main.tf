95a02e8668da446585620963a7523d004f7a27c2
surendra1302 <surendrakorivi96@gmail.com>
2025-03-12 17:41:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
 
-module "EKS-cluster" {
-    source = "./EKS-cluster"
+module "EKS_cluster" {
+    source = "./EKS_cluster"
     cluster_name = var.name
     subnet_ids = var.subnet_ids
 }
 
-module "EKS-nodegroup"{
-    source = "./EKS-nodegroup"
-    cluster_name = module.EKS-cluster.cluster_name
+module "EKS_nodegroup"{
+    source = "./EKS_nodegroup"
+    cluster_name = module.EKS_cluster.cluster_name
     desired_size = "4"
-    maxvalue = "20"
+    maxvalue = "15"
     minvalue = "3"
     instance_type = "t3.medium"
     subnet_ids = var.subnet_ids
-    depends_on = [ module.EKS-cluster ]
+    depends_on = [ module.EKS_cluster ]
 }
